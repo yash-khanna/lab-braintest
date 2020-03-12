43e4e584acae410de28ae6603c58925ebaaee9b9
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-12 12:14:57 +0530

    Update PersonalityCalculator.java

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,11 +1,2 @@
 package service;
 
-// Create a class called PersonalityCalculator
-// PersonalityCalculator has two methods findAnswers and findYourBrainType
-// int[] findAnswers(String options) and String findYourBrainType(String options) is the method prototype
-// findAnswer() accepts String as an argument and returns integer array as an output
-// findYourBrainType accepts String as an argument and returns String as an output
-// option is a string which contains all the options selected as a string
-// Use the split method to segregate the options and store it in a string array.
-// Convert the string array into an integer array.
-// return the entire integer array to findyourBrainType
